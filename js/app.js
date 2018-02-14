67cf4a1a704139f1fa1ddb90bb15e5171c59b47c
newgermes <kukhar.vitalii@gmail.com>
2018-02-14 14:09:41 +0200

    add addScript function

--- js/app.js
+++ js/app.js
@@ -2,14 +2,26 @@
     var src = {
         'scripts': [
             {'src': 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', 'async': false},
-            {'src': './modules/cart.js', 'async': false},
-            {'src': './modules/main.js', 'async': false}
+            {'src': './js/modules/cart.js', 'async': false},
+            {'src': './js/modules/main.js', 'async': false}
         ]
     };
 
     var addScript = function(src) {
         if (toString.call(src) !== "[object Array]") return fasle;
 
-        
+        for (var i = 0; i < src.length; i++) {
+            var script = document.createElement("script"), 
+                item = src[i];
+
+            script.src = item.src;
+            script.async = item.async;
+            document.body.appendChild(script);
+        }
+        return true;
     };
+
+    window.addEventListener('load', function() {
+        addScript(src.scripts);
+    });
 } ());
