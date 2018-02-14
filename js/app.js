3605c79ce800fb954775c6a6f5e8d188806fc90f
newgermes <kukhar.vitalii@gmail.com>
2018-02-14 13:54:00 +0200

    add script

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function() {
+    var src = {
+        'scripts': [
+            {'src': 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', 'async': false},
+            {'src': './modules/cart.js', 'async': false},
+            {'src': './modules/main.js', 'async': false}
+        ]
+    };
+
+    var addScript = function(src) {
+        if (toString.call(src) !== "[object Array]") return fasle;
+
+        
+    };
+} ());
---
+++ js/modules/cart.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Cart = (function() {
+    var start = function() {
+        
+    };
+
+    return {
+        start: start
+    };
+} ());
