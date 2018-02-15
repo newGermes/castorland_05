e8da214ce35e1dc701e1f2392e317ad1a996df1a
newgermes <kukhar.vitalii@gmail.com>
2018-02-15 12:29:51 +0200

    add delegation logic

--- js/controllers/addToCart.js
+++ js/controllers/addToCart.js
@@ -2,9 +2,32 @@
 // import shorCart from shortCart
 
 app.addToCart = (function() {
+    var _parser = function(element) {
+        var name = element.dataset.name;
+        var id = element.dataset.id;
+        var url = '';
+        var price = 0;
+        var quantity = 0;
+
+        return {
+            id: id,
+            url: url,
+            name: name,
+            quantity: quantity,
+            price: price
+        }
+    };
     var delegation = function(parent, child) {
         if (parent && child) {
-            
+            var reg = new RegExp(child.className, 'i');
+            parent.onclick = function(e) {
+                e.preventDefault();
+                var elm = e.target;
+
+                if (!reg.test(elm.className)) return;
+
+                console.log(_parser(elm).id);
+            };
         }
     };
     
